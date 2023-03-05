9228c5a7ada5d8778e4c5e78d3f63e4117c46339
jescobarca <79493851+jescobarca@users.noreply.github.com>
2023-03-04 21:57:34 -0500

    Add files via upload

---
+++ n14/src/main/java/com/mycompany/n14/N14.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.n14;
+
+/**
+ *
+ * @author jorge
+ */
+public class N14 {
+    
+    public static void main(String[] args) {
+        double a=48,br,ret,nt;
+        br=a*5000;
+        ret=br*0.125;
+        nt=br-ret;
+        System.out.println("Salario Bruto:"+br);
+        System.out.println("Retencion:"+ret);
+        System.out.println("Salario neto:"+nt);
+    }
+}
